31d4c524a99c3cc9342936a470b1b613564f8686
vivivi <v.castro05@ufromail.cl>
2024-10-09 15:19:17 -0300

    initialize component structure with buttons and display areas

---
+++ src/components/APIData.vue
@@ -0,0 +1,26 @@
+<template>
+    <button >Random Manga</button>
+    <button >Random Anime</button>
+    <button >Random Country</button>
+    <button >Random Anime Recomendations</button>
+
+    <div v-if="randomManga">
+        <h3>Random Manga</h3>
+        <p>{{ randomManga }}</p>
+    </div>
+
+    <div v-if="randomAnime">
+        <h3>Random Anime</h3>
+        <p>{{ randomAnime }}</p>
+    </div>
+
+    <div v-if="countriesPopulation">
+        <h3>Countries Population</h3>
+        <p>{{ countriesPopulation }}</p>
+    </div>
+
+    <div v-if="animeRecomendations">
+        <h3>Anime Recomendations</h3>
+        <p>{{ animeRecomendations }}</p>
+    </div>
+</template>
